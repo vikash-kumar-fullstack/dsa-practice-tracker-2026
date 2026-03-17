6a4345a6cac329342cd1223e3a0c209683b8c91a
Vikash Kumar <nisant54321@gmail.com>
2026-03-17 11:51:32 +0530

    understanding about the the multiple pattern problems

---
+++ pattern.cpp
@@ -0,0 +1,659 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // to print this 
+    // * * * 
+    //   * *
+    //     *
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<rows-i;k++){
+    //         cout<<"*";
+    //     }cout<<endl;
+    // }
+
+    // to print this
+    // 1 2 3
+    //   1 2
+    //     1
+
+    //  int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<rows-i;k++){
+    //         cout<<k+1;
+    //     }cout<<endl;
+    // }
+
+    // to print this
+    // *
+    // * *
+    // * * *
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<"*";
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // 1
+    // 1 2
+    // 1 2 3
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<j+1;
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // 1 2 3
+    // 1 2
+    // 1 
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i;j++){
+    //         cout<<j+1;
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // * * *
+    // * *
+    // * 
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i;j++){
+    //         cout<<"*";
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    //     *
+    //   * *
+    // * * *
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int k=0;k<i+1;k++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // to print this
+    //     1
+    //   1 2
+    // 1 2 3
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int k=0;k<i+1;k++){
+    //         cout<<k+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // to print this
+    // *  *  *  *
+    // *        *
+    // *        *
+    // *  *  *  *
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows;j++){
+    //         if(j==0 || i==0 || i==rows-1 || j==rows-1){
+    //             cout<<"* ";
+    //         }else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // to print this
+    //       *
+    //      * *
+    //     * * *
+    //    * * * *
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<i+1;k++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // to print this
+    //    * * * *
+    //     * * *
+    //      * *
+    //       *
+
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<rows-i;k++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // to print this
+    //       *
+    //      * *
+    //     * * *
+    //    * * * *  
+    //    * * * *
+    //     * * *
+    //      * *
+    //       *
+
+    // int rows,ro;
+    // cout<<"enter number of rows:";
+    // cin>>ro;
+    // if(ro%2==0){
+    //     rows=ro/2;
+    // }else{
+    //     rows=(ro+1/2);
+    // }
+    //     for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<i+1;k++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=0;k<rows-i;k++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    
+
+    // to print this
+    //       *
+    //     *   *
+    //   *       *
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<" ";
+    //     }cout<<"*";
+    //     for(int k=0;k<(2*i-1);k++){
+    //         cout<<" ";
+    //     }if(i>0){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // } 
+    
+
+    // to print this
+    //   *       *
+    //     *   *
+    //       *
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //    for(int j=0;j<2*rows-1;j++){
+    //     if(i-j==0 || i+j==2*rows-2){
+    //         cout<<"* ";
+    //     }else{
+    //         cout<<"  ";
+    //     }
+    //    }
+    //     cout<<endl;
+    // } 
+
+
+    // to print this
+    //       *
+    //     *   *
+    //   *       *
+    //     *   *
+    //       *
+    // int rows;
+    // cout<<"enter number of rows in odd only:";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows;j++){
+    //         if(j+i==rows/2 || j-i==rows/2 || i-j==rows/2 || i+j==3*(rows/2)){
+    //             cout<<"* ";
+    //         }else{
+    //             cout<<"  ";
+    //         }
+    //     }cout<<endl;
+    // }
+
+
+
+    // to print this
+    //   * * *   * * *
+    //   * *       * *
+    //   *           *
+    //   *           *
+    //   * *       * *
+    //   * * *   * * *
+    // int rows;
+    // cout<<"enter number of rows:";
+    // cin>>rows;
+    // int half=rows/2;
+    // for(int i=0;i<half;i++){
+    //     for(int k=0;i+k<half;k++){
+    //             cout<<"* ";
+    //     }
+    //     for(int l=0;l<2*i+1;l++){
+    //         cout<<"  ";
+    //     }
+    //     for(int m=0;m+i<half;m++){
+    //             cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<half;i++){
+    //     for(int k=0;k<i+1;k++){
+    //             cout<<"* ";
+    //     }
+    //     for(int l=0;l<2*(half-i)-1;l++){
+    //         cout<<"  ";
+    //     }
+    //     for(int m=0;m<i+1;m++){
+    //             cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // to print this
+    // 1
+    // 2 * 2
+    // 3 * 3 * 3
+    // 4 * 4 * 4 * 4
+    // int rows;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i< rows;i++){
+    //     for(int j=0;j<2*i+1;j++){
+    //         if(j%2==0){
+    //             cout<< i+1<<" ";
+    //         }else{
+    //             cout<<"* ";
+    //         }
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // 1
+    // 2 * 2
+    // 3 * 3 * 3
+    // 4 * 4 * 4 * 4
+    // 3 * 3 * 3
+    // 2 * 2 
+    // 1
+    // int rows;
+    // cout<<"enter the number of rows in odd only:";
+    // cin>>rows;
+    // for(int i=0;i< rows/2+1;i++){
+    //     for(int j=0;j<2*i+1;j++){
+    //         if(j%2==0){
+    //             cout<< i+1<<" ";
+    //         }else{
+    //             cout<<"* ";
+    //         }
+    //     }cout<<endl;
+    // }
+    // for(int i=0;i<rows/2;i++){
+    //     for(int j=0;j<2*(rows/2-i)-1;j++){
+    //          if(j%2==0){
+    //             cout<< rows/2-i<<" ";
+    //         }else{
+    //             cout<<"* ";
+    //         }
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // * * * * *
+    // *     *
+    // *   *
+    // * *
+    // *
+    // int rows;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i;j++){
+    //         if(i==0 || j==0 || j==rows-i-1){
+    //             cout<<"* ";
+    //         }else{
+    //             cout<<"  ";
+    //         }
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // 1
+    // 1 * 2
+    // 1 * * 3
+    // 1 * * * 4
+    // 1 2 3 4 5
+    // int rows;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     if(i==0 || i == rows-1){
+    //         for(int j=0;j<i+1;j++){
+    //             cout<< j+1<<" ";
+    //     }
+    //     }else{
+    //         for(int j=0;j<i+2;j++){
+    //                 if(j==0){
+    //                     cout<<"1 ";
+    //                 }
+    //                 else if(j==i+1){
+    //                     cout<< j;
+    //                 }else{
+    //                     cout<<"* ";
+    //                 }
+    //             }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // to print
+    // 1
+    // 1 2 1
+    // 1 2 3 2 1
+    // 1 2 3 4 3 2 1
+    // 1 2 3 4 5 4 3 2 1
+
+    // int rows;
+    // int t=0;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<2*i+1;j++){
+    //         if(j<i+1){
+    //             char ch=j+1+'a'-1;
+    //             cout<<ch<<" ";
+    //         }else{
+    //             char ch=i-t+'a'-1;
+    //             cout<<ch<<" ";
+    //             t++;
+    //         }
+    //     }t=0;cout<<endl;
+    // }
+
+    // to print this
+    // 1 2 3 4 5
+    // 2     5
+    // 3   5
+    // 4 5
+    // 5
+
+    // int rows;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     if(i==0){
+    //       for(int j=0;j<rows-i;j++){
+    //         cout<<j+1<<" ";
+    //     }  
+    //     }else{
+    //         for(int j=0;j<rows-i;j++){
+    //             if(j==0)cout<<i+1<<" ";
+    //             else if(j+i==rows-1)cout<<rows<<" ";
+    //             else{
+    //                 cout<<"  ";
+    //             }
+    //         }
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // * * * * 1
+    // * * * 1 2 1
+    // * * 1 2 3 2 1
+    // * 1 2 3 4 3 2 1
+    // 1 2 3 4 5 4 3 2 1
+
+    // int rows;
+    // int k;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<"* ";
+    //     }
+    //     for(k=0;k<i+1;k++){
+    //         cout<<k+1<<" ";
+    //     }
+    //     for(int l=k-1;l>0;l--){
+    //         cout<<l<<" ";
+    //     }cout<<endl;
+    // }
+
+
+    // to print this
+    // * * * * * * * * 1 * * * * * * * *
+    // * * * * * * * 2 * 2 * * * * * * *
+    // * * * * * * 3 * 3 * 3 * * * * * *
+    // * * * * * 4  * 4 * 4 * 4 * * * * *
+    // * * * * 5 * 5 * 5 * 5 * 5 * * * *
+
+    // int rows;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int k=0;k<2*rows-i-2;k++){
+    //         cout<<"* ";
+    //     }
+    //     for(int l=0;l<i+1;l++){
+    //         cout<<i+1<<" * ";
+    //     }
+    //     for(int m=0;m<2*rows-i-3;m++){
+    //         cout<<"* ";
+    //     }cout<<endl;
+    // }
+
+
+
+    // to print this
+    // * 
+    // * * 
+    // * * * 
+    // * * * * 
+    // * * *
+    // * * 
+    // *
+
+    // int rows;
+    // int k;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     if(i<rows/2+1){
+    //         for(int j=0;j<i+1;j++){
+    //             cout<<"* ";
+    //         }
+    //     }else{
+    //         for(int k=rows-i;k>0;k--){
+    //             cout<<"* ";
+    //         }
+    //     }cout<<endl;
+    // }
+
+
+
+    // to print floyd triangle
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    // 11 12 13 14 15
+
+    // int rows;
+    // int k=0;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<k+1<<" ";k++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    // to print this
+    // * _ _ _ _ _ _ *
+    // * * _ _ _ _ * * 
+    // * * * _ _ * * *
+    // * * * * * * * *
+    // * * * * * * * *
+    // * * * _ _ * * *
+    // * * _ _ _ _ * *
+    // * _ _ _ _ _ _ *
+
+    // int rows;
+    // cout<<"enter the number of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     if(i<rows/2){
+    //         for(int j=0;j<i+1;j++){
+    //             cout<<"* ";
+    //         }
+    //         for(int l=0;l<2*(rows/2-i-1);l++){
+    //             cout<<"  ";
+    //         }
+    //         for(int m=0;m<i+1;m++){
+    //             cout<<"* ";
+    //         }
+    //     }else{
+    //         for(int k=rows-i;k>0;k--){
+    //             cout<<"* ";
+    //         }
+    //         for(int l=0;l<2*i-rows;l++){
+    //             cout<<"  ";
+    //         }
+    //         for(int k=rows-i;k>0;k--){
+    //             cout<<"* ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // to print this
+    //         1
+    //       1 2 1
+    //     1 2 3 2 1
+    //   1 2 3 4 3 2 1
+    // 1 2 3 4 5 4 3 2 1
+
+    // int rows;
+    // cout<<"enter no of rows :";
+    // cin>>rows;
+    // for(int i=0;i<rows;i++){
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int k=0;k<i+1;k++){
+    //         cout<<k+1<<" ";
+    //     }
+    //     for(int k=i;k>0;k--){
+    //         cout<<k<<" ";
+    //     }
+    //     for(int j=0;j<rows-i-1;j++){
+    //         cout<<"  ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    
+    // to print this
+    // * * * * *
+    // * *   * *
+    // *   *   *
+    // * *   * *
+    // * * * * *
+
+
+
+
+
+
+
+
+}
