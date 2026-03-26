72c5531d7cb9ee5e840bf00e0ffdc03eff5dead9
Vikash Kumar <nisant54321@gmail.com>
2026-03-26 22:00:31 +0530

    understanding the basic opertion of queue using defined ds of queue

---
+++ queue.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+
+// using defined ds of queue
+
+// int main(){
+//     queue<int>q;
+//     q.push(10);
+//     q.push(20);
+//     q.push(30);
+//     q.push(40);
+//     q.push(50);
+
+//     cout<<"the top element of queue is :"<<q.front()<<endl;
+//     cout<<"the total size of queue is :"<<q.size()<<endl;
+
+//     q.pop();
+//     cout<<"after pop the isize of queue is :";
+//     cout<<"the total size of queue is :"<<q.size()<<endl;
+//     cout<<"the rear element of the queue is :"<<q.back()<<endl;
+// }
+
